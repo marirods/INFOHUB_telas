217deeffbd0d7882cbf649de774041a358326cff
MARIANE DA SILVA RODRIGUES <24122311@dsn127m1246732.cfp127aluno.local>
2025-09-16 15:49:44 -0300

    Criando a Tela cadastro adicionando img

--- app/src/main/java/com/example/infohub_telas/telas/TelaCadastro.kt
+++ app/src/main/java/com/example/infohub_telas/telas/TelaCadastro.kt
@@ -1,4 +1,46 @@
-package com.example.infohub_telas.telas
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.infohub_telas.R
+import com.example.infohub_telas.ui.theme.InfoHub_telasTheme
 
-class TelaCadastro {
+
+@Composable
+fun TelaCadastro() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.cadastro), // usa cadastro.png
+            contentDescription = "Cadastro",
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(220.dp),
+            contentScale = ContentScale.Fit
+        )
+
+    }
+}
+
+
+@Preview(showSystemUi = true)
+@Composable
+fun TelaCadastroPreview() {
+    InfoHub_telasTheme {
+        TelaCadastro()
+    }
 }
