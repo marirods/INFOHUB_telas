12cb79277b8d694a2af5ce94268dd24d8dad7153
marirods <mariane.rodrigues933@gmail.com>
2025-09-23 15:47:42 -0300

    tela redefinicao de senha criada

--- app/src/main/java/com/example/infohub_telas/telas/TelaRedefinicaoSenha.kt
+++ app/src/main/java/com/example/infohub_telas/telas/TelaRedefinicaoSenha.kt
@@ -1,2 +1,53 @@
 package com.example.infohub_telas.telas
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.infohub_telas.ui.theme.InfoHub_telasTheme
+
+@Composable
+@OptIn(ExperimentalMaterial3Api::class)
+fun TelaRedefinicaoSenha() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White)
+    ) {
+        Column {
+
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(360.dp)
+                    .background(Color(0xFFFFD966))
+            ) {
+
+            }
+
+
+        }
+    }
+}
+
+
+
+
+
+
+@Preview(showSystemUi = true)
+@Composable
+fun TelaRedefinicaoSenhaPreview() {
+    InfoHub_telasTheme {
+        TelaRedefinicaoSenha()
+    }
+}
+
