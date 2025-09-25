fa2f6f7036d5035de2d8143891af92a32367ddcb
MARIANE DA SILVA RODRIGUES <24122311@CFP127ALUNO.LOCAL>
2025-09-25 17:22:14 -0300

    integracao api

---
+++ app/src/main/java/com/example/infohub_telas/model/recuperarSenha.kt
@@ -0,0 +1,9 @@
+package com.example.infohub_telas.model
+
+data class recuperarSenha(
+
+    val email: String,
+
+)
+
+
--- app/src/main/java/com/example/infohub_telas/telas/TelaRedefinicaoSenha.kt
+++ app/src/main/java/com/example/infohub_telas/telas/TelaRedefinicaoSenha.kt
@@ -1,6 +1,7 @@
 package com.example.infohub_telas.telas
 
 import android.content.Context
+import android.util.Patterns
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
@@ -18,6 +19,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Email
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextFieldDefaults
@@ -40,7 +42,13 @@ import com.example.infohub_telas.R
 import com.example.infohub_telas.ui.theme.InfoHub_telasTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.ui.platform.LocalContext
+import com.example.infohub_telas.model.Usuario
+import com.example.infohub_telas.model.recuperarSenha
 import com.example.infohub_telas.service.RetrofitFactory
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import retrofit2.await
 
 @Composable
 @OptIn(ExperimentalMaterial3Api::class)
@@ -54,7 +62,10 @@ fun TelaRedefinicaoSenha() {
     val retrofitFactory = RetrofitFactory()
     val userApi = retrofitFactory.getInfoHub_UserService()
 
-
+    fun validar(): Boolean{
+        val emailCliente = !Patterns.EMAIL_ADDRESS.matcher(email).matches()
+        return emailCliente
+    }
 
 
     Box(
@@ -177,8 +188,18 @@ fun TelaRedefinicaoSenha() {
             Button(
                 onClick = {
                     //validacao dos dados
-                    if (email.isNotEmpty()){
-                        isLoading = true
+                    if (validar()){
+                        val emailEnviado = recuperarSenha(
+                            email = email
+                        )
+
+                        //requisição do POST para a API
+                        GlobalScope.launch(Dispatchers.IO){
+                            val novoUsuario = userApi.recuperarSenha(emailEnviado).await()
+                            println(novoUsuario)
+                        }
+                    }else{
+                        println("********* DADOS INCORRETOS! PREENCHER CORRETAMENTE*****************")
                     }
                 },
                 modifier = Modifier
@@ -188,13 +209,15 @@ fun TelaRedefinicaoSenha() {
                 shape = RoundedCornerShape(28.dp),
                 colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF25992E))
             ) {
-                Text(
-                    text = "Recuperar",
-                    color = Color.White,
-                    fontSize = 18.sp,
-                    fontWeight = FontWeight.Bold
-                )
+                if (isLoading) {
+                    CircularProgressIndicator(color = Color.White, modifier = Modifier.size(24.dp))
+                } else {
+                    Text("Recuperar", color = Color.White, fontSize = 18.sp, fontWeight = FontWeight.Bold)
+                }
+
+
             }
+
         }
     }
 }
